2504ba4bd0ece6e5371f0cde5cc610184102cc44
Phu La <la_phu@yahoo.com>
2022-12-12 19:18:17 +0000

    Sample file added.

---
+++ lap_fib_py/__init__.py
@@ -0,0 +1,2 @@
+def say_hello() -> None:
+    print("the LaP Fibonacci PY module is saying hello")
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+
+with open("README.md", "r") as fh:
+    long_description_txt = fh.read()
 setup(
     name="lap_fib_py",
     version="0.0.1",
     author="Phu La",
     author_email="la_phu@yahoo.com",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description_txt,
     long_description_content_type="text/markdown",
     url="https://github.com/qdl-lap/lap-fib-py",
     install_requires=[],
